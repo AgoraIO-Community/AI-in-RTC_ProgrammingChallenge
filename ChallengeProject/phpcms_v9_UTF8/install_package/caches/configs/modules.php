0575bd051fcfdee20aec5cd30450ea85c3ed5166
Yuanxingqian <844800670@qq.com>
2019-09-30 10:15:14 +0800

    phpcms安装包
    先上传phpcms安装包，按照安readme安装phpcms

---
+++ ChallengeProject/phpcms_v9_UTF8/install_package/caches/configs/modules.php
@@ -0,0 +1,13 @@
+<?php
+return array (
+  'member' => '会员',
+  'vote' => '投票',
+  'link' => '友情链接',
+  'message' => '短消息',
+  'announce' => '网站公告',
+  'special' => '专题',
+  'content' => '内容模块',
+  'block' => '碎片',
+  'comment' => '评论',
+);
+?>
---
+++ ChallengeProject/phpcms_v9_UTF8/install_package/phpsso_server/caches/configs/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'port' => 3306,
+		'database' => 'phpsso',
+		'username' => '',
+		'password' => '',
+		'tablepre' => 'ps_',
+		'charset' => 'gbk',
+		'type' => 'mysqli',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		)
+);
+
+?>
